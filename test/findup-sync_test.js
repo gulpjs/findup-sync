4e39fc8f6dd061b738f23790a0161627f167c879
Simo Kinnunen <simo@shoqolate.com>
2012-11-15 15:14:55 -0500

    Fix: Ensure API can be used without an options object (closes #1)

--- test/findup-sync_test.js
+++ test/findup-sync_test.js
@@ -12,7 +12,11 @@ var rel = function(abspath) {
 
 exports['findup'] = {
   setUp: function(done) {
-    // setup here
+    this.cwd = process.cwd();
+    done();
+  },
+  tearDown: function(done) {
+    process.chdir(this.cwd);
     done();
   },
   'simple': function(test) {
@@ -26,6 +30,19 @@ exports['findup'] = {
     test.equal(rel(findup(['b*.txt', 'f*.txt'], opts)), 'test/fixtures/a/b/bar.txt', 'should find the first thing that matches any of the glob patterns');
     test.equal(rel(findup(['f*.txt', 'b*.txt'], opts)), 'test/fixtures/a/b/bar.txt', 'should find the first thing that matches any of the glob patterns');
     test.equal(findup('not-gonna-exist-i-hope.txt', opts), null, 'should returning null if no files found');
+    test.done();
+  },
+  'cwd': function(test) {
+    test.expect(8);
+    process.chdir('test/fixtures/a/b');
+    test.equal(rel(findup('foo.txt')), '../foo.txt', 'should find files');
+    test.equal(rel(findup('bar.txt')), 'bar.txt', 'should find files');
+    test.equal(rel(findup('a.txt')), '../../a.txt', 'should find files');
+    test.equal(rel(findup('?.txt')), '../../a.txt', 'should support glob patterns');
+    test.equal(rel(findup('*.txt')), 'bar.txt', 'should find the first thing that matches the glob pattern');
+    test.equal(rel(findup(['b*.txt', 'f*.txt'])), 'bar.txt', 'should find the first thing that matches any of the glob patterns');
+    test.equal(rel(findup(['f*.txt', 'b*.txt'])), 'bar.txt', 'should find the first thing that matches any of the glob patterns');
+    test.equal(findup('not-gonna-exist-i-hope.txt'), null, 'should returning null if no files found');
     test.done();
   },
 };
