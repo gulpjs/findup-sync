a40f8d8d2d6af7c78bb45a7490891e38ed4b2d3b
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-03 22:18:48 -0500

    use latest code from look-up

--- index.js
+++ index.js
@@ -7,82 +7,77 @@
 var fs = require('fs');
 var path = require('path');
 var isGlob = require('is-glob');
+var resolveDir = require('resolve-dir');
 var mm = require('micromatch');
-var dir = process.cwd();
 
 /**
- * Expose `findup`
- */
-
-module.exports = findup;
-
-/**
- * @param  {String|Array} `patterns` Glob pattern(s) or file path(s) to match against.
- * @param  {Object} `options` Options to pass to [micromatch]. Note that if you want to start in a different directory than the current working directory, specify a `cwd` property here.
+ * @param  {String|Array} `pattern` Glob pattern or file path(s) to match against.
+ * @param  {Object} `options` Options to pass to [micromatch]. Note that if you want to start in a different directory than the current working directory, specify the `options.cwd` property here.
  * @return {String} Returns the first matching file.
  * @api public
  */
 
-function findup(patterns, options) {
-  if (typeof patterns !== 'string' && !Array.isArray(patterns)) {
-    throw new TypeError('findup expects a string or array as the first argument.');
+module.exports = function(patterns, options) {
+  if (typeof patterns === 'string') {
+    return lookup(patterns, options);
   }
 
-  // ensure the pattern is an array
-  patterns = typeof patterns === 'string'
-    ? [patterns]
-    : patterns;
-
-  var opts = options || {};
-  var cwd = opts.cwd || process.cwd();
-  var len = patterns.length;
-
-  // loop over patterns
-  while (len--) {
-    var pattern = patterns[len];
-
-    // if the pattern is a glob pattern, move on
-    if (!isGlob(pattern)) {
-
-      // if the pattern is not a glob pattern, try
-      // to see if it resolves to an actual file so
-      // we can avoid using fs.readdir and matching
-      var file = path.join(cwd, pattern);
-      if (fs.existsSync(file)) {
-        return file;
-      }
-    }
-
-    if (!/\*\*/.test(pattern)) {
-      opts.matchBase = true;
-    }
+  if (!Array.isArray(patterns)) {
+    throw new TypeError('findup-sync expects a string or array as the first argument.');
   }
 
+  var len = patterns.length, i = -1;
+  while (++i < len) {
+    var res = lookup(patterns[i], options);
+    if (res) return res;
+  }
+
+  return null;
+};
+
+function lookup(pattern, options) {
+  options = options || {};
+  var cwd = resolveDir(options.cwd || '');
+  if (isGlob(pattern)) {
+    return matchFile(cwd, pattern, options);
+  } else {
+    return findFile(cwd, pattern);
+  }
+}
+
+function matchFile(cwd, pattern, opts) {
+  var isMatch = mm.matcher(pattern, opts);
   var files = fs.readdirSync(cwd);
-  var flen = files.length;
+  var len = files.length, i = -1;
 
-  // loop through the files in the current directory
-  while (flen--) {
-    var fp = path.join(cwd, files[flen]);
-
-    // if the current directory is the actual cwd, break out
-    if (path.dirname(fp) === '.') { break; }
-
-    // if the file path matches the pattern(s), return it
-    var match = mm(fp, patterns, opts);
-    if (match.length !== 0) {
+  while (++i < len) {
+    var name = files[i];
+    var fp = path.join(cwd, name);
+    if (isMatch(name) || isMatch(fp)) {
       return fp;
     }
   }
 
-  // nothing was matched in the last dir, so move up a
-  // directory and create a new `cwd` for the search
-  cwd = path.resolve(cwd, '..');
+  var dir = path.dirname(cwd);
+  if (dir === cwd) return null;
+  return matchFile(dir, pattern, opts);
+}
 
-  // we're past the actual cwd with no matches.
-  if (cwd === dir) { return null; }
+function findFile(cwd, filename) {
+  var fp = cwd ? (cwd + '/' + filename) : filename;
+  if (fs.existsSync(fp)) {
+    return fp;
+  }
 
-  // try again
-  opts.cwd = cwd;
-  return findup(patterns, opts);
+  var segs = cwd.split(path.sep);
+  var len = segs.length;
+
+  while (len--) {
+    cwd = segs.slice(0, len).join('/');
+    fp = cwd + '/' + filename;
+    if (fs.existsSync(fp)) {
+      return fp;
+    }
+  }
+  return null;
 }
